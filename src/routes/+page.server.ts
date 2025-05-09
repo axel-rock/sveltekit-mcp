fd32c1d8142ad0a520ad50e7c1695268a205745d
axel-rock <rockaxel@gmail.com>
2025-05-09 09:55:47 +0200

    fix: Make URL dynamic

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,22 +1,20 @@
 import { createAnthropic } from '@ai-sdk/anthropic'
 import { PRIVATE_ANTHROPIC_KEY } from '$env/static/private'
-import { experimental_createMCPClient, generateText, streamText } from 'ai'
+import { experimental_createMCPClient, generateText } from 'ai'
 import { StreamableHTTPClientTransport } from '@modelcontextprotocol/sdk/client/streamableHttp.js'
 
 const anthropic = createAnthropic({ apiKey: PRIVATE_ANTHROPIC_KEY })
 const model = anthropic('claude-3-7-sonnet-20250219')
 
 export const actions = {
-	mcp: async ({ request }) => {
-		console.log('MCP')
+	mcp: async ({ request, url }) => {
 		const formData = await request.formData()
 		const prompt = formData.get('prompt') as string
-		const transport = new StreamableHTTPClientTransport(new URL('http://localhost:5173/mcp'))
+		const transport = new StreamableHTTPClientTransport(new URL(`${url.origin}/mcp`))
 		const customClient = await experimental_createMCPClient({
 			transport,
 		})
 		const tools = await customClient.tools()
-		console.dir(tools, { depth: null })
 
 		const gen = await generateText({
 			model,
